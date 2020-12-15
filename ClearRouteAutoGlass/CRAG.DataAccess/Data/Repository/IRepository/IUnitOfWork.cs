d90d3d13bcbfe9a2e74e2a6f0d6a16337a7751dc
Raul Perez <ralberto73@gmail.com>
2020-12-15 17:06:53 -0500

    Created files for Repository Pattern

---
+++ ClearRouteAutoGlass/CRAG.DataAccess/Data/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRAG.DataAccess.Data
+{
+    interface IUnitOfWork
+    {
+    }
+}
---
+++ ClearRouteAutoGlass/CRAG.Models/SystemSetting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CRAG.Models
+{
+    public class SystemSetting
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        [Display(Name ="Key")]
+        public int Key { get; set; }
+
+        [Display(Name = "Value")]
+        public int Value  { get; set; }
+    }
+}
