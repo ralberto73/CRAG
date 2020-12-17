8e5befb123f5c3fa44bd9f635a10f9b76a6ea046
Raul Perez <ralberto73@gmail.com>
2020-12-16 22:48:07 -0500

    Ado Repository First Aproach

---
+++ ClearRouteAutoGlass/CRAG.DataAccess/Data/Repository/ADORepository.cs
@@ -0,0 +1,83 @@
+﻿using CRAG.DataAccess.Data.Repository.IRepository;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+
+namespace CRAG.DataAccess.Data.Repository
+{
+    public class ADORepository<T, K> : IADOReepository<T, K> where T : class , new() 
+    {
+
+        private string        _connection_string;
+        private SqlConnection _connection = null;
+        public ADORepository(string connection_string)
+        {
+            _connection_string = connection_string;
+            _connection = new SqlConnection(connection_string);
+        }
+
+        private SqlConnection DBConnection
+        {
+            get
+            {
+                if  (_connection.State == ConnectionState.Connecting)
+                {
+                    while (_connection.State == ConnectionState.Connecting)
+                        Thread.Sleep(1000);
+                }
+                if (_connection.State != ConnectionState.Open)
+                    _connection.Open();
+
+                return _connection;
+            }
+        }
+        public T Get( string procedure_name , SqlParameter sql_parameter , K id)
+        {
+            T result = null;
+            using (SqlCommand cmd = new SqlCommand(procedure_name, DBConnection))
+            {
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.Add(sql_parameter);
+                cmd.Parameters[procedure_name].Value = id;
+                using (var reader = cmd.ExecuteReader())
+                {
+                    if  (reader.Read())
+                    {
+                         result = MapValues<T>(reader);
+                    }
+                }
+            }
+            return result;
+        }
+
+        private T MapValues<T1>(SqlDataReader reader) 
+        {
+            T result = new T();
+            // var columnShemas = ;
+
+            foreach (var schema in reader.GetColumnSchema())
+            {
+                var property_name = schema.ColumnName.Trim();
+                var type = result.GetType();
+                // Get the PropertyInfo object by passing the property name.
+                PropertyInfo myPropInfo = type.GetProperty(property_name);
+                if (myPropInfo != null)
+                    // Fill  the property.
+                    myPropInfo.SetValue(result, reader[property_name], null);
+            }
+            return result;
+        }
+
+
+
+        public IEnumerable<T> GetAll(List<SqlParameter> sql_parameters, params object[] params_values)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ ClearRouteAutoGlass/CRAG.DataAccess/Data/Repository/IRepository/IADOReepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRAG.DataAccess.Data.Repository.IRepository
+{
+    /// <summary>
+    ///   Signature of Data repository 
+    /// </summary>
+    /// <typeparam name="T">Model's Class</typeparam>
+    /// <typeparam name="K">Primary Key Type </typeparam>
+    public interface  IADOReepository<T, K> where T : class
+    {
+
+        /// <summary>
+        ///   Call  Stored procedure  [Identity]_GetById 
+        /// </summary>
+        /// <param name="id">Primary Key</param>
+        /// <param name="procedure_Name">Procedure Name</param>
+        /// <returns></returns>
+        public T Get(string procedure_name, SqlParameter sql_parameter, K id);
+
+
+        /// <summary>
+        /// Get Stored Procedure
+        /// </summary>
+        /// <param name="sql_parameters">List of parameters </param>
+        /// <param name="params_values">List of Values</param>
+        /// <returns></returns>
+        IEnumerable<T> GetAll(List<SqlParameter> sql_parameters, params object[] params_values);
+    }
+}
