0c061b8eca65e434f61fd9fe1fa9318ed23aea25
Raul Perez <ralberto73@gmail.com>
2020-12-19 21:08:32 -0500

    Fixed int of Work

--- ClearRouteAutoGlass/CRAG.DataAccess/Data/Repository/ADOUnitOfWork.cs
+++ ClearRouteAutoGlass/CRAG.DataAccess/Data/Repository/ADOUnitOfWork.cs
@@ -6,11 +6,13 @@ namespace CRAG.DataAccess.Data.Repository
 {
     public class ADOUnitOfWork : IADOUnitOfWork
     {
-        public IBrandRepository Brands { get; set; }
+        public IBrandRepository     Brands { get; set; }
+        public IInsuranceRepository Insurances { get; set; }
 
         public ADOUnitOfWork( string connection_String)
         {
             Brands = new BrandRepository(connection_String);
+            Insurances  = new InsuranceRepository(connection_String);
         }
 
         public void Dispose()
--- ClearRouteAutoGlass/CRAG.Utility/SQL_Scripts/01 - Create Tables.sql
+++ ClearRouteAutoGlass/CRAG.Utility/SQL_Scripts/01 - Create Tables.sql
@@ -1,105 +1,36 @@
 USE [CRAG]
 GO
 
-ALTER TABLE [dbo].[Brands] DROP CONSTRAINT [DF_Brands_LastUpdateDate]
+--- Entity :  WorkOrderStatuses
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[WorkOrderStatuses]') AND type in (N'U'))
+BEGIN
+  ALTER TABLE [dbo].[WorkOrderStatuses] DROP CONSTRAINT [DF_WorkOrderStatuses_LastUpdateDate]
+  ALTER TABLE [dbo].[WorkOrderStatuses] DROP CONSTRAINT [DF_WorkOrderStatuses_CreationDate]
+  ALTER TABLE [dbo].[WorkOrderStatuses] DROP CONSTRAINT [DF_WorkOrderStatuses_UpdatedBy]
+  ALTER TABLE [dbo].[WorkOrderStatuses] DROP CONSTRAINT [DF_WorkOrderStatuses_CreatedBy]
+END
 GO
-
-ALTER TABLE [dbo].[Brands] DROP CONSTRAINT [DF_Brands_CreationDate]
+DROP TABLE IF EXISTS [dbo].[WorkOrderStatuses]
 GO
-
-ALTER TABLE [dbo].[Brands] DROP CONSTRAINT [DF_Brands_UpdatedBy]
-GO
-
-ALTER TABLE [dbo].[Brands] DROP CONSTRAINT [DF_Brands_CreatedBy]
-GO
-
-/****** Object:  Table [dbo].[Brands]    Script Date: 12/18/2020 9:22:53 PM ******/
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Brands]') AND type in (N'U'))
-DROP TABLE [dbo].[Brands]
-GO
-
-/****** Object:  Table [dbo].[Brands]    Script Date: 12/18/2020 9:22:53 PM ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE TABLE [dbo].[Brands](
-	[BrandId] [int] IDENTITY(1,1) NOT NULL,
-	[BrandName] [nvarchar](50) NULL,
-	[CreatedBy] [varchar](100) NULL,
-	[UpdatedBy] [varchar](100) NULL,
-	[CreationDate] [datetime] NOT NULL,
-	[LastUpdateDate] [datetime] NOT NULL,
- CONSTRAINT [PK_Brands] PRIMARY KEY CLUSTERED 
+CREATE TABLE dbo.WorkOrderStatuses
 (
-	[BrandId] ASC
+	[WorkOrderStatusId]      INT IDENTITY(1,1)   NOT NULL,
+	[WorkOrderStatusName]    VARCHAR     (50)    NOT NULL,
+	[CreatedBy]        VARCHAR     (100)   NOT NULL,
+	[UpdatedBy]        VARCHAR     (100)   NOT NULL,
+	[CreationDate]     DATETIME            NOT NULL,
+	[LastUpdateDate]   DATETIME            NOT NULL,
+ CONSTRAINT [PK_WorkOrderStatuses] PRIMARY KEY CLUSTERED 
+(
+	[WorkOrderStatusId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-
-ALTER TABLE [dbo].[Brands] ADD  CONSTRAINT [DF_Brands_CreatedBy]  DEFAULT ('Anonymous') FOR [CreatedBy]
+ALTER TABLE [dbo].[WorkOrderStatuses] ADD  CONSTRAINT [DF_WorkOrderStatuses_CreatedBy]  DEFAULT ('Anonymous') FOR [CreatedBy]
 GO
-
-ALTER TABLE [dbo].[Brands] ADD  CONSTRAINT [DF_Brands_UpdatedBy]  DEFAULT ('Anonymous') FOR [UpdatedBy]
+ALTER TABLE [dbo].[WorkOrderStatuses] ADD  CONSTRAINT [DF_WorkOrderStatuses_UpdatedBy]  DEFAULT ('Anonymous') FOR [UpdatedBy]
 GO
-
-ALTER TABLE [dbo].[Brands] ADD  CONSTRAINT [DF_Brands_CreationDate]  DEFAULT (getdate()) FOR [CreationDate]
+ALTER TABLE [dbo].[WorkOrderStatuses] ADD  CONSTRAINT [DF_WorkOrderStatuses_CreationDate]  DEFAULT (getdate()) FOR [CreationDate]
 GO
-
-ALTER TABLE [dbo].[Brands] ADD  CONSTRAINT [DF_Brands_LastUpdateDate]  DEFAULT (getdate()) FOR [LastUpdateDate]
+ALTER TABLE [dbo].[WorkOrderStatuses] ADD  CONSTRAINT [DF_WorkOrderStatuses_LastUpdateDate]  DEFAULT (getdate()) FOR [LastUpdateDate]
 GO
----    Insurances
-
-
-ALTER TABLE [dbo].[Insurances] DROP CONSTRAINT [DF_Insurances_LastUpdateDate]
-GO
-
-ALTER TABLE [dbo].[Insurances] DROP CONSTRAINT [DF_Insurances_CreationDate]
-GO
-
-ALTER TABLE [dbo].[Insurances] DROP CONSTRAINT [DF_Insurances_UpdatedBy]
-GO
-
-ALTER TABLE [dbo].[Insurances] DROP CONSTRAINT [DF_Insurances_CreatedBy]
-GO
-
-/****** Object:  Table [dbo].[Insurances]    Script Date: 12/18/2020 9:22:53 PM ******/
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Insurances]') AND type in (N'U'))
-DROP TABLE [dbo].[Insurances]
-GO
-
-/****** Object:  Table [dbo].[Insurances]    Script Date: 12/18/2020 9:22:53 PM ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE TABLE [dbo].[Insurances](
-	[InsuranceId] [int] IDENTITY(1,1) NOT NULL,
-	[InsuranceName] [nvarchar](50) NULL,
-	[CreatedBy] [varchar](100) NULL,
-	[UpdatedBy] [varchar](100) NULL,
-	[CreationDate] [datetime] NOT NULL,
-	[LastUpdateDate] [datetime] NOT NULL,
- CONSTRAINT [PK_Insurances] PRIMARY KEY CLUSTERED 
-(
-	[InsuranceId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-
-ALTER TABLE [dbo].[Insurances] ADD  CONSTRAINT [DF_Insurances_CreatedBy]  DEFAULT ('Anonymous') FOR [CreatedBy]
-GO
-
-ALTER TABLE [dbo].[Insurances] ADD  CONSTRAINT [DF_Insurances_UpdatedBy]  DEFAULT ('Anonymous') FOR [UpdatedBy]
-GO
-
-ALTER TABLE [dbo].[Insurances] ADD  CONSTRAINT [DF_Insurances_CreationDate]  DEFAULT (getdate()) FOR [CreationDate]
-GO
-
-ALTER TABLE [dbo].[Insurances] ADD  CONSTRAINT [DF_Insurances_LastUpdateDate]  DEFAULT (getdate()) FOR [LastUpdateDate]
-GO
-
